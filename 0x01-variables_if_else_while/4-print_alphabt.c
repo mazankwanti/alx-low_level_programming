c7a1ba09a268bd903f777d44304bbbd2abea7d7c
mazankwanti <johnpaulmazankwanti7052@gmail.com>
2022-09-10 18:21:28 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print if the number is positive, zero, or negaative 
+ * Return; 0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if (ch != 'e' && ch != 'q')
+{
+	putchar(ch);
+}
+}
+putchar('\n');
+return (0);
