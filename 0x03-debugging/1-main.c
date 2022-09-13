4fc8c00fedb5b1fce7e6fa234cdad10f0d43f3b9
mazankwanti <johnpaulmazankwanti7052@gmail.com>
2022-09-13 12:35:31 -0700

    task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *Return: 0
+ */
+int main(void)
+{
+int i;
+printf("infinite loop incoming :("\n");
+
+1 = 0;
+
+/*
+ * while (i = 10);
+ * infite loop = noNo increase of variable
+ * putchar (0);
+ * }
+ */
+printF("infinite loop  avoided! //o\n");
+return (0);
+}
