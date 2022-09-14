52224109617f8b064a0441d125ff41ddbb842d57
mazankwanti <johnpaulmazankwanti7052@gmail.com>
2022-09-14 05:00:28 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet
+ *
+ * Return: always success
+ */
+void print_alphabet(void)
+{
+int n;
+for (n = 'a'; n <= '2' ; n++)
+{
+putchar(n);
+}
+putchar("\n");
+}
+
